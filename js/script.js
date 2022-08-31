e61659c05364cad143868cbcfbb68b51e33af2fc
MargaritaVin <ritasergienko@rambler.ru>
2022-08-31 15:51:44 +0300

    added function getUsers in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+async function getUsers() {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!response.ok) {
+        throw new Error("Error: not found");
+      }
+      const data = await response.json();
+      const user = data.find(({company: {name}}) => name === "Johns Group");
+      console.log(user);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  getUsers();
+  
